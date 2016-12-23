3200fb16de3dcf2db503780d0fb5fdd182a89a1c
Yura <yura.paray@gmail.com>
2016-12-24 00:01:31 +0200

    create portfoliojson

--- app/js/app.js
+++ app/js/app.js
@@ -1,9 +1,15 @@
 (function(){
-  
+
   //carousel for home page
   $('.carousel').carousel();
 
   //mobile menu
   $(".button-collapse").sideNav();
+
+  $.getJSON("../data/portfolio.json", function(json) {
+    console.log("ok");
+    console.log(json);
+    alert('sds');
+  });
 
 })();
